7a191559861cbbf6ec1f7faf2ef0514f31ceae6a
hsdistefa <hsdistefa@gmail.com>
2017-04-13 12:07:03 -0700

    Added K nearest neighbors classification

---
+++ k_nearest_neighbors.py
@@ -0,0 +1,51 @@
+import numpy as np
+import scipy.stats
+
+
+class KNN():
+    def __init__(self, k):
+        self.k = k
+
+    def predict(self, X_train, y_train, X_test):
+        # TODO: implement KNN regression
+        # For each sample in the test set, find the k closest samples in the
+        # train set and the corresponding labels
+        labels_matrix = np.empty((X_test.shape[0], self.k))
+        labels_matrix[:] = np.NAN
+        for (i, test_sample) in enumerate(X_test):
+            knn_dist = np.full(self.k, np.inf)
+            knn_labels = np.empty(self.k)
+            knn_labels[:] = np.NAN
+
+            max_neighbor_i = 0
+            for (j, train_sample) in enumerate(X_train):
+                dist = self._euclidean_distance(test_sample, train_sample)
+                if dist < knn_dist[max_neighbor_i]:
+                    knn_dist[max_neighbor_i] = dist
+                    knn_labels[max_neighbor_i] = y_train[j]
+                    max_neighbor_i = np.argmax(knn_dist)
+            labels_matrix[i] = knn_labels
+
+        # Take the majority vote of each of the k closest train samples to
+        # determine the label to apply to each of the test samples
+        modes, _ = scipy.stats.mode(labels_matrix, axis=1)
+        return modes
+
+    def _euclidean_distance(self, x1, x2):
+        return np.linalg.norm(x1 - x2)
+
+
+def test():
+    NUM_SAMPLES = 100
+    NUM_FEATURES = 4
+    np.random.seed(1005)
+    X_train = np.random.randn(NUM_SAMPLES, NUM_FEATURES)
+    y_train = np.random.randint(0, NUM_FEATURES+1, size=NUM_SAMPLES)
+    X_test = np.array([[2, 2, 7, 3],
+                      [-1, 5, 6, 10]])
+    knn = KNN(2)
+    print(knn.predict(X_train, y_train, X_test))
+
+
+if __name__ == '__main__':
+    test()
