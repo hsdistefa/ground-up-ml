4489a1814d05f8ef15a4b36ccc0bf542f09fec7e
hsdistefa <hsdistefa@gmail.com>
2017-08-18 20:57:30 -0700

    Added regression to KNN

--- supervised/k_nearest_neighbors.py
+++ supervised/k_nearest_neighbors.py
@@ -16,14 +16,18 @@ from pca import PCA
 
 
 class KNN():
-    def __init__(self, k):
+    def __init__(self, k, method):
         """K Nearest Neighbors
 
         Args:
             k (int):
                 Number of neighbors to use when evaluating test points
+            method (str):
+                class: Classification
+                reg:   Regression
         """
         self.k = k
+        self.method = method
 
     def predict(self, X_train, y_train, X_test):
         """Using the k nearest neighbors in the given training data, lazy
@@ -36,8 +40,12 @@ class KNN():
                 Training labels
             X_test (numpy array of shape [n_samples, n_features]):
                 Test data
+
+        Returns:
+            C (numpy array of shape [n_samples]):
+                Predicted values from test data
         """
-        # TODO: implement KNN regression
+        # TODO: implement Kernel Regression (locally weighted)
         # For each sample in the test set, find the k closest samples in the
         # train set and the corresponding labels
         labels_matrix = np.empty((X_test.shape[0], self.k))
@@ -56,17 +64,25 @@ class KNN():
                     max_neighbor_i = np.argmax(knn_dist)
             labels_matrix[i] = knn_labels
 
-        # Take the majority vote of each of the k closest train samples to
-        # determine the label to apply to each of the test samples
-        modes, _ = scipy.stats.mode(labels_matrix, axis=1)
-        return modes.T
+        if self.method == 'class':
+            # Take the majority vote of each of the k closest train samples to
+            # determine the label to apply to each of the test samples
+            predictions, _ = scipy.stats.mode(labels_matrix, axis=1)
+            predictions = predictions.T
+
+        elif self.method == 'reg':
+            # Take the average of each of the k closest train samples to
+            # determine the values to apply to the test samples.
+            predictions = np.mean(labels_matrix, axis=1)
+
+        return predictions
 
     def _euclidean_distance(self, x1, x2):
         return np.linalg.norm(x1 - x2)
 
 
 def test():
-    np.random.seed(seed=777)
+    np.random.seed(seed=7778)
 
     # Load dataset and normalize
     iris = sklearn.datasets.load_iris()
@@ -94,7 +110,7 @@ def test():
     y_test = y[split_index:]
 
     # Run the model
-    knn = KNN(2)
+    knn = KNN(2, 'class')
     y_pred = knn.predict(X_train, y_train, X_test)
 
     # Compute the model error
