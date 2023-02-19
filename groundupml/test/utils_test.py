dde22d9bbec76b5d2700e0018d99991bf4bb2193
hsdistefa <hsdistefa@gmail.com>
2023-02-18 20:02:25 -0800

    Added utils distribution poisson test multiple input parameters equal to

--- groundupml/test/utils_test.py
+++ groundupml/test/utils_test.py
@@ -4,8 +4,18 @@ import pytest
 from groundupml.utils import distributions
 
 
-def test_poisson():
-    lmda = 1
-    k = 0
-    assert distributions.poisson(lmda, k) == pytest.approx(0.368, rel=1e-3)
+test_poisson_input = [(1, 0, 0.368),
+                      (1, 1, 0.368),
+                      (1, 2, 0.184),
+                      (1, 3, 0.06131),
+                      (1, 4, 0.01533),
+                      (1, 5, 0.003066),
+                      (2.5, 0, 0.0821),
+                      (2.5, 1, 0.2052),
+                      (2.5, 2, 0.2565)
+                     ]
+
+@pytest.mark.parametrize("lmda,k,expected", test_poisson_input)
+def test_poisson_equal(lmda, k, expected):
+    assert distributions.poisson(lmda, k) == pytest.approx(expected, rel=1e-3)
 
