93923b2b5566bbd91b6cebe54eeda41bba6407bd
hsdistefa <hsdistefa@gmail.com>
2023-04-13 11:32:20 -0700

    Added combined softmax with cross entropy neural network activation layer

--- groundupml/neuralnetwork/activations.py
+++ groundupml/neuralnetwork/activations.py
@@ -2,7 +2,8 @@ from __future__ import print_function, division
 
 import numpy as np
 
-from groundupml.utils.functions import sigmoid, sigmoid_prime, softmax
+from groundupml.utils.functions import cross_entropy, sigmoid, sigmoid_prime, \
+                                       softmax
 
 
 class SigmoidLayer():
@@ -102,12 +103,6 @@ class SoftmaxLayer():
     a sample belongs to each possible class. For example, 2% chance of being a
     duck, 18% chance of being a dog, 80% chance of being a cat. Notice that the
     sum of the probabilities is 1.
-
-    Args:
-        n_nodes (int):
-            Number of nodes in layer.
-        n_inputs (int):
-            Number of inputs to layer.
     """
     def __init__(self):
         self.inputs = None
@@ -163,3 +158,67 @@ class SoftmaxLayer():
         d_softmax = self.activations * (1 - self.activations)
 
         return gradient * d_softmax
+
+class SoftmaxCrossEntropyLayer():
+    """Softmax Cross Entropy Layer
+
+    This layer is intented for use **only** as the final layer in a neural
+    network. It combines the softmax activation function with cross entropy
+    loss. This combination makes calculation of the gradients much more
+    simple and efficient than when using a general softmax layer separately.
+    
+    These two functions are very commonly used together in multiclass
+    classification because the softmax creates a distribution (where
+    probabilities sum to 1) over the classes, and the cross entropy loss
+    requires a probability distribution as input.
+    """
+    def __init__(self):
+        self.activations = None
+        self.cross_entropy_loss = None
+        self.targets = None
+
+    def forward_propogate(self, X):
+        """Propogate input forward through layer
+
+        Args:
+            X (numpy array of shape [n_samples, n_features]):
+                Input data
+
+        Returns:
+            numpy array of shape [n_classes, 1]:
+                Activated output where values correspond to softmax activations
+        """
+        self.activations = softmax(X)
+
+        return self.activations
+
+    def back_propogate(self, targets):
+        """Propogate error gradient backward through layer using chain rule
+
+        Args:
+            targets (numpy array of shape [n_samples, n_classes]):
+                One-hot encoded target classes
+
+        Returns:
+            numpy array of shape [n_samples, n_inputs]:
+                Error gradient for layer below
+        """
+        # Gradient derived here: https://eli.thegreenplace.net/2016/the-softmax-function-and-its-derivative/
+        return (self.activations - targets) / self.activations.shape[0]
+
+
+    def calculate_loss(self, actual):
+        """Calculates the cross entropy loss for the current activations
+
+        Args:
+            targets (numpy array of shape [n_samples, n_classes]):
+                One-hot encoded target classes
+
+        Returns:
+            numpy array of shape [n_samples, 1]:
+                Cross entropy loss for each sample
+        """
+        # TODO: Make this work for multiple samples
+        self.cross_entropy_loss = cross_entropy(self.activations, actual)
+
+        return self.cross_entropy_loss
